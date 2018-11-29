85ef458835c769792c960fb1e02edf92ae2f5e90
pmate955 <pmate955@gmail.com>
2018-11-29 16:48:48 +0100

    registration is working

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema.define(version: 2018_11_13_143909) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "firstName", default: "", null: false
+    t.string "lastName"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
