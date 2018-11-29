21bee3255dea91be51567b4a62277bd172db63ab
pmate955 <pmate955@gmail.com>
2018-11-29 17:29:31 +0100

    Login form updated

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
    #GET /resource/sign_up
-   def new
-     super
-   end
+  # def new
+  #   super
+  # end
 
   # POST /resource
   # def create
@@ -41,14 +41,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+   def configure_sign_up_params
+
+     #params.require(:user).permit(:firstName, :lastName, :email, :password, :password_confirmation)
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:firstName, :lastName, :email, :password, :password_confirmation])
+     #devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+   def configure_account_update_params
+     #params.require(:user).permit(:firstName, :lastName, :email, :password, :password_confirmation, :current_password)
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:firstName, :lastName, :email, :password, :password_confirmation])
+   end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'user/login'
   get 'user/register'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'home#index'
