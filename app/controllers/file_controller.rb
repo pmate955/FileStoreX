31bd83eefbaec64c86dc0e858f424743af2c1291
pmate955 <pmate955@gmail.com>
2018-11-30 22:17:06 +0100

    file read is working

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -1,15 +1,20 @@
 class FileController < ApplicationController
+  @uploadedFile
+  @lines
+
   def upload
   end
 
   def uploadFile
     if params[:passw1] == params[:passw2]
-      if params[:isEncrypt] == 1
-        encryptFile(params[:selectedFile], params[:passw1])
+      if params[:isEncrypt] == 'true'
+        saveFile(params[:selectedFile])
+      #  encryptFile(params[:selectedFile], params[:passw1])
+      #  puts(@lines)
       end
 
     else
-      redirect_back fallback_location: '/file/upload',  flash: {result: 'Passwords are bad'}
+      redirect_back fallback_location: '/file/upload',  flash: {result: 'Passwords are not same!'}
     end
   end
 
@@ -24,8 +29,33 @@ protected
   end
 
   private
-  def encryptFile(file, pass)
 
+  def saveFile(upload)
+    name = upload['datafile'].original_filename
+    directory = "public/data"
+    # create the file path
+    path = File.join(directory, name)
+    # write the file
+   # puts(upload['datafile'].read)
+    contents = IO.binread(upload['datafile'].path)
+    puts(contents)
+    for pos in 0...contents.length
+      puts contents[pos].
+    end
+      #File.open(path, "wb") { |f| f.write(upload['datafile'].read) }
+  end
+
+  def encryptFile(file, pass)
+    puts('Encrypt')
+    @uploadedFile = file
+    if  @uploadedFile.respond_to?(:read)
+      @lines = file_data.read
+    elsif  @uploadedFile.respond_to?(:path)
+      @lines = File.read(file_data.path)
+    else
+      logger.error "Bad file_data: #{ @uploadedFile.class.name}: #
+    {@filename.inspect}"
+    end
   end
 
 end
