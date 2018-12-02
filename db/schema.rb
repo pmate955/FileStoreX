bc5672cba07952835f9d8675bf52f93d54fb92d6
pmate955 <pmate955@gmail.com>
2018-12-02 23:24:27 +0100

    Set to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_12_02_155947) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "file_items", force: :cascade do |t|
     t.string "password"
     t.string "path"
